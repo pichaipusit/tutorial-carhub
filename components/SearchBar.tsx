05ccf4545de0d63838be28dfa9af4e77b82f66f6
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-09-14 06:18:35 +0700

    prob: SearchButton

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,12 +2,35 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import SearchManufacturer from "./SearchManufacturer";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
 
+  // TODO: How can u update url when users press the search btn?
   const handleSearch = () => {};
+
+  const updateSearchParams = () => {
+    // Create a new URLSearchParams object using the current URL search parameters
+    // Update or delete the 'model' search parameter based on the 'model' value
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+    // Generate the new pathname with the updated search parameters
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -16,6 +39,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManuFacturer={setManuFacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
       <div className="searchbar__item">
         <Image
@@ -25,6 +49,15 @@ const SearchBar = () => {
           className="absolute w-[20px] h-[20px] ml-4"
           alt="car model"
         />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+        />
+        {/* TODO: Code below so that There're 2 search btns on mobile view, but 1 on desktop view  */}
       </div>
     </form>
   );
